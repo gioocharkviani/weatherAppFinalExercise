829eb99b57be3c17a8a5f01417191f7955677d8f
gioocharkviani <gioocharkviani@gmail.com>
2023-01-18 17:57:21 +0400

    finish geo location

--- script.js
+++ script.js
@@ -16,15 +16,16 @@ AddBtn.addEventListener('click' , e => {
 function Fetch() { 
     const key = 'ce0227bf1465d8cfc1b69e68343e0ba8';
     const GeoChordUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${Cityinput.value}&limit=1&appid=${key}`;
+    let lat;
+    let lon;
+    const losdsdsada = [];
     fetch(GeoChordUrl)
     .then(res =>  res.json())
     .then(data => {
-        
-        for(let GeoLoc in data){
-            let location = data[GeoLoc]
-            console.log(location);
-        }
-        
+        console.log(data[0])
+        lat = data[0].lat;
+        lon = data[0].lon;
+        console.log(lat, lon)
 })
 
 }
