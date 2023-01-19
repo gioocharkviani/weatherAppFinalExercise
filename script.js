98ba66c96edacf05cb0c3e9f51a164f1a4a0bac4
Ana Charkviani <acharkviani@evolution.com>
2023-01-20 02:09:43 +0400

    gtydfyt

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const aspan = document.querySelector('#aspan');
 const Cityinput = document.querySelector('#Cityinput');
 const Add = document.querySelector('#Add');
 const Content = document.querySelector(".Content")
+const Addbox = document.querySelector(".Addbox")
+
+
+
+
+//==================week days =============
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const newdate = new Date();
+let day = weekday[newdate.getDay()];
+//==================week days =============
 
 let GeographicLocation = JSON.parse(localStorage.getItem('Locations'))  || [];
 
@@ -28,12 +38,8 @@ function Fetch() {
 
 
 
-//==========================ADD Box==========================
-
-//==========================ADD Box==========================
-
 // GetRequest Loop
-async function GetrequestLoop() {
+function GetrequestLoop() {
 
     GeographicLocation.forEach((location) => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat1}&lon=${location.lon1}&appid=${key}`)
@@ -46,53 +52,65 @@ async function GetrequestLoop() {
             let wthdesc = data.weather[0].description; 
             let wethIcon = data.weather[0].icon; 
             let city = location.city;
-            console.log(wethIcon);
 
 //===================RENDER FUNCTION=======================
-Content.innerHTML += `
-    <div class="Box WeatherCard">
 
-        <header class="cardHeader">
-            <div class="Wthnow"><img src="http://openweathermap.org/img/wn/${wethIcon}.png"  alt="sun" /></div>
-            <div class="Cityinfo">
-            <h2>${city}</h2>
-            <span>Monday 01/17/2022</span>
-            </div>
-            </header>
-                        
-        <div class="WthCelsius">
-            <div class="celsius">
-            <h2>${Math.floor(temp)}</h2>
-            <span>°C</span>
-            </div>
-            <div class="whetherinfo">${wthdesc}</div>
-        </div>
-                        
-        <div class="wthinfo">
-            <li><img src="./images/hum.svg" alt="hum" />Humidity<span>${Math.floor(Humidity)}</span></li>
-             <li class="borderline"><img src="./images/wind.svg" alt="wind" />Feels like<span>${Math.floor(Feelslike)}</span></li>
-            <li><img src="./images/visibility.svg" alt="hum" />Visibility<span>${Math.floor(visibility)}</span></li>
-            <li class="borderline"><img src="./images/temp.svg" alt="wind" />temp<span>${Math.floor(temp)}</span></li>
-        </div>
-                        
-    </div>   ` ;
-//===================RENDER FUNCTION=======================
-                                       
+let CreatBox = document.createElement('div');
+CreatBox.setAttribute('class' , 'Box WeatherCard');
+CreatBox.setAttribute('id' , city);
+Content.appendChild(CreatBox);
+
+
+CreatBox.innerHTML = `
+
+<div class="removecard">
+<button id="removecard" data-action="remove" onclick="removeFunction()"><img src="./images/remove.svg" /></button>
+</div>
+
+<header class="cardHeader">
+<div class="Wthnow"><img src="http://openweathermap.org/img/wn/${wethIcon}.png"  alt="sun" /></div>
+<div class="Cityinfo">
+<h2>${city}</h2>
+<span>${day}</span>
+</div>
+</header>
+            
+<div class="WthCelsius">
+<div class="celsius">
+<h2>${Math.floor(temp)}</h2>
+<span>°C</span>
+</div>
+<div class="whetherinfo">${wthdesc}</div>
+</div>
+            
+<div class="wthinfo">
+<li><img src="./images/hum.svg" alt="hum" />Humidity<span>${Math.floor(Humidity)}</span></li>
+<li class="borderline"><img src="./images/wind.svg" alt="wind" />Feels like<span>${Math.floor(Feelslike)}</span></li>
+<li><img src="./images/visibility.svg" alt="hum" />Visibility<span>${Math.floor(visibility)}</span></li>
+<li class="borderline"><img src="./images/temp.svg" alt="wind" />temp<span>${Math.floor(temp)}</span></li>
+</div>
+`
+//===================RENDER FUNCTION=======================                                     
         })
+        
     });
     
 }
 
 GetrequestLoop()
-// GetRequest Loop
 
 
 Add.addEventListener('click' , e => {
     e.preventDefault();
     if (Cityinput.value == '') {
-        console.log('Insert City name');
+        alert('Insert City name');
     }else{
         Fetch();
+        setTimeout(() => {
+            GetrequestLoop();
+            location.reload();
+          }, 1000)
+          
     }
 })
 
@@ -101,6 +119,20 @@ AddBtn.addEventListener('click' , () => {
     InputBox.style = 'display:block';
     AddBtn.style = 'display:none';
     aspan.style = 'display:none';
-    console.log(e);
 })
 
+
+function removeFunction(city){
+    let temp = GeographicLocation.filter(location => location.city != city)
+    localStorage.setItem('Locations', JSON.stringify(GeographicLocation));
+}
+
+Content.addEventListener('click', (event) => {
+    const target = event.target;
+    const ParentElement = target.parentNode;
+
+    const task = ParentElement;
+    const taskId = Number(task.id);
+
+    console.log(target);
+})
